8b331406acd6d20c53628f4b32556cc4ef5649d6
jsherbert36 <54801612+jsherbert36@users.noreply.github.com>
2020-03-14 17:23:17 +0000

    add tests

---
+++ Tests/call_phone.py
@@ -0,0 +1,24 @@
+import serial
+import time
+phone_num = input('Enter phone number: ')
+ser = serial.Serial(port = "/dev/ttyUSB0", baudrate = 115200)
+W_buf_logoin = "AT+CREG?\r\n"
+W_buf_phone =  "ATD" + phone_num+ ";\r\n"
+ser.write(W_buf_logoin.encode())
+ser.flushInput()
+data = ""
+try:
+        while True:
+		while ser.inWaiting() > 0:
+			data += (ser.read(ser.inWaiting())).decode()
+			time.sleep(0.0001)
+		if data != "":
+			print(data)
+			if "CREG" in data:
+				print("call phone")
+				ser.write(W_buf_phone.encode())
+			data = ""
+except KeyboardInterrupt:
+	if ser != None:
+		ser.close()
+		
